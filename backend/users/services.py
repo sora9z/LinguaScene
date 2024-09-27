ad5effea45153a76375db52061c9d275ce057990
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-09-27 15:36:07 +0900

    [LinguaScene/backend/users] view, service 로직 분리-login service 로직 추가

--- backend/users/services.py
+++ backend/users/services.py
@@ -1,7 +1,8 @@
 from asyncio import exceptions
 import logging
 
-from .serializers import SignupSerializer
+from .serializers import LoginSerializer, SignupSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 logger = logging.getLogger(__name__)
@@ -20,5 +21,22 @@ def signup_service(data):
         logger.error(f"[user/service] Error during signup: {e}")
         raise e
 
+def login_service(data):
+    try:
+        serializer = LoginSerializer(data=data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            # token 생성
+            refresh = RefreshToken.for_user(user)
+            refresh['email'] = user.email
 
-            
+            return {
+                'refreshToken': str(refresh),
+                'accessToken': str(refresh.access_token),
+            }
+    except exceptions.InvalidStateError as e:
+        logger.error(f"[user/service] Invalid state error: {e}")
+        raise e
+    except Exception as e:
+        logger.error(f"[user/service] Unexpected error: {e}")
+        raise e
