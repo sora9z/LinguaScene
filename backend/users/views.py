4fe5b7821a9dc00bab80d5f624cc623c41277de8
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-09-27 17:38:46 +0900

    [LinguaScene/backend/users] view, service 로직 분리-login view 리팩토링

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .services import signup_service
+from .services import login_service, signup_service
 
 from .serializers import LoginSerializer, SignupSerializer
 
@@ -28,26 +28,13 @@ class SignupView(APIView):
 class LoginView(APIView):
     permission_classes = [AllowAny]
     def post(self,request):
-     serializer = LoginSerializer(data=request.data, context={'request': request})
-
-     if serializer.is_valid():
-        user = serializer.validated_data['user']
-        
-        # user 인증
-        logger.debug(f"Authentication result: user={user}")
-        
-        if user is not None:
-            # token 생성
-            refresh = RefreshToken.for_user(user)
-            refresh['email'] = user.email
-
-            return Response({
-                'refreshToken': str(refresh),
-                'accessToken': str(refresh.access_token),
-
-            },status = status.HTTP_200_OK)
-        
-        logger.error(f"Serializer errors: {serializer.errors}")
-        return Response({'error':'Invalid credentials'},status = status.HTTP_401_UNAUTHORIZED)
+        try: 
+            result = login_service(request.data)
+            logger.info("User login successfully")
+            return Response(result,status = status.HTTP_200_OK)
+        except exceptions.InvalidStateError:  
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)  
+        except Exception as e:
+            return Response({"detail": "Server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
