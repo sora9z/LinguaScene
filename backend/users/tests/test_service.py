53be53bae7d4a22a25ab7c45988efe990c717948
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-09-27 15:35:27 +0900

    [LinguaScene/backend/users] view, service 로직 분리- 회원가입, login test code 추가

--- backend/users/tests/test_service.py
+++ backend/users/tests/test_service.py
@@ -1,26 +1,96 @@
+from asyncio import exceptions
+from unittest import mock
+from unittest.mock import MagicMock, Mock, patch
 from django.test import TestCase
-from unittest.mock import patch, MagicMock
-from users.services import signup_service
 
-class UserServiceTest(TestCase):
-    @patch('users.services.SignupSerializer')  # signup_service에서 사용된 SignupSerializer를 모킹
-    def test_signup_user_seccess(self,MockSignupSerializer):
-        # Mocking
-        mock_serializer = MockSignupSerializer.return_value
+from users.services import login_service, signup_service
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserServiceTests(TestCase):
+    @patch('users.services.SignupSerializer') # serializer mocking
+    def test_signup_servlice_success(self,MockSignuupSerializer):
+
+        mock_serializer = MockSignuupSerializer.return_value
         mock_serializer.is_valid.return_value = True
-        mock_user = MagicMock(username="testuser")
+        mock_user = MagicMock(email='testuser@example.com') # fake user instance
         mock_serializer.save.return_value = mock_user
 
         data = {
-            "email":"test@test.com",
-            "password":"1234"
+            "password": "password123",
+            "email": "testuser@example.com"
         }
+
+        user = signup_service(data)
+
+        self.assertEqual(user.email,mock_user.email)
+
+        mock_serializer.is_valid.assert_called_once()
+        mock_serializer.save.assert_called_once()
+    
+    @patch('users.services.SignupSerializer')
+    def test_signup_service_exception_invali(self,MockSignupSerializer):
+        mock_serializer = MockSignupSerializer.return_value
+        mock_serializer.is_valid.return_value = False
+        mock_serializer.errors = {'nor_field_errors':['Invalid data']}
+
+        data = {
+            "username": "testuser",
+            "password": "password123",
+            "email": "testuser@example.com"
+        }
+
+        with self.assertRaises(exceptions.InvalidStateError): 
+            signup_service(data)
+
+        mock_serializer.is_valid.assert_called_once()
+
+    
+    @patch('users.services.LoginSerializer')
+    @patch('users.services.RefreshToken')
+    def test_login_service_success(self,MockRefreshToken,MockLoginSerializer):
+        mock_serializer = MockLoginSerializer.return_value
+        mock_serializer.is_valid.return_value = True
+        mock_user = MagicMock(email='testuser@example.com') # fake user instance
+        mock_serializer.validated_data = {'user': mock_user} 
+
+        mock_refresh_instance = MockRefreshToken.for_user.return_value
+        mock_refresh_instance.access_token = 'fake_access_token'
+        mock_refresh_instance.__str__.return_value = 'fake_refresh_token'
+
+        data = {
+            "password": "password123",
+            "email": "testuser@example.com"
+        }
+
+        result = login_service(data)
+
+        mock_serializer.is_valid.assert_called_once()
+        self.assertEqual(result['refreshToken'],'fake_refresh_token')
+        self.assertEqual(result['accessToken'],'fake_access_token')
+
+    @patch('users.services.LoginSerializer')
+    def test_login_service_exception_invalid(self,MockingLoginSerializer):
+        mock_serializer = MockingLoginSerializer.return_value
+        mock_serializer.is_valid.return_value = True
+        mock_serializer.validated_data = {'user': None} 
+        mock_serializer.errors = {'nor_field_errors':['Invalid credentials']}
+
+        data = {
+            "password": "password123",
+            "email": "testuser@example.com"
+        }
+
+        with self.assertRaises(exceptions.InvalidStateError):
+            login_service(data)
         
-        signup_service(None,data)
+        mock_serializer.is_valid.assert_called_once()
 
-        mock_serializer.assert_called_once_witn(data=data)
-        mock_serializer.return_value.save.assert_called_once()
-  
-
+    
+    
 
         
+
+        
+
