9bd0bbef45d0b56d62ef47e50b3c80b7f9906392
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-10-03 00:38:49 +0900

    [LinguaScene/backend] langchain적용하기 - openai call by langchain 추가

--- backend/chat/services/openai_service_langchain.py
+++ backend/chat/services/openai_service_langchain.py
@@ -1,12 +1,12 @@
 import os
 
-from chat.services.langchain_service.chains import ChatCain
+from chat.services.langchain_service.chains import ChatChain
 
 
-class OPenAIService:
+class OPenAIServiceWithLangChain:
     def __init__(self):
         self.api_key = os.getenv("OPENAI_API_KEY")
-        self.chat_chain = ChatCain(api_key = self.api_key)
+        self.chat_chain = ChatChain(api_key = self.api_key)
 
     async def def_chat_response(self,messages):
         response = self.chat_chain.get_response(messages)
