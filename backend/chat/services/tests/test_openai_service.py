7d3b533e29e0a499ed9e633930317b56581971e1
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-10-07 10:45:31 +0900

    [LinguaScene/backend] langchain적용하기 - 테스트코드 구조 수정 및 openai service 테스트코드 추가

---
+++ backend/chat/services/tests/test_openai_service.py
@@ -0,0 +1,27 @@
+
+from unittest.mock import patch
+
+from chat.services.openai_service_langchain import OPenAIServiceWithLangChain
+
+
+class OPenAIServiceTestCase:
+
+    # api key를 로드하는가
+    # chat_chain을 사용해서 메시지에 대한 응답을 받는가
+    # chat_chain의 method를 모의하여 예상대로 응답을 처리하는지 확인
+    @patch('chat.services.langchain_service.chains.ChatChain')
+    @patch('os.getenv')
+    async def test_get_chat_response(mock_getenv,mock_chat_chain):
+        mock_getenv.return_value = 'test_api_key'
+        mock_chat_chain_instance = mock_chat_chain.return_value
+        mock_chat_chain_instance.get_response.return_value = 'mocked response'
+
+        service = OPenAIServiceWithLangChain()
+        messages = [{'role':'user','content':'hello'}]
+        response = service.def_chat_response(messages)
+
+        # assertion
+        mock_getenv.assert_called_once_with("OPENAI_API_KEY")
+        mock_chat_chain.assert_called_once_with(api_key='test_api_key')
+        mock_chat_chain_instance.get_response.assert_called_once_with(messages)
+        assert response == 'mocked response'
