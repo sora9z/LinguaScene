e36372bf998997eb89b99639b578b4e628e228d0
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-10-09 01:07:49 +0900

    [LinguaScene/backend] 리팩토링 - 구조수정 - message model에서 role 관리하도록 추가
    - user_message, system_message,assistant_message를 database에서 관리하도록 수정

--- backend/message/models.py
+++ backend/message/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from chat.models import ChatRoom
 
+
 class Message(models.Model):
+    ROLE_CHOICES = [
+        ('user','User'),
+        ('system','System'),
+        ('assistant','Assistant'),
+    ]
+
     chat_room = models.ForeignKey(ChatRoom,related_name='messages', on_delete=models.CASCADE)
+    role = models.CharField(max_length=10,choices=ROLE_CHOICES)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
